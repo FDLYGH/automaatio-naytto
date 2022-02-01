650f4c3877adc75f5a86c9a15e6eba126077146d
Lari Tolvanen <lari.tolvanen@edu.sasky.fi>
2022-02-01 15:34:50 +0200

    test2

--- naytto/src/App.js
+++ naytto/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Testi muutos2</h1>
+        <h1>Testi muutos2434423</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
